8e826d68017441b1040c1d2ce5c4e9363917b031
Boje <jacob.boje@cgi.com>
2022-12-14 22:11:30 +0100

    PoC

--- App.js
+++ App.js
@@ -5,7 +5,12 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Oh well</Text>
+      <input name='myinput'></input>
       <StatusBar style="auto" />
+      <svg>
+        <rect width={100} height={100} fill="yellow"></rect>
+      </svg>
     </View>
   );
 }
